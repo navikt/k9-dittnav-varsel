4607bc8f34ad55755a29597b0d365d12c98af74e
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-09 10:31:31 +0200

    Fiks navn

--- src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
@@ -2,6 +2,6 @@ package no.nav.sifinnsynapi.config
 
 object Topics {
     const val K9_DITTNAV_VARSEL_BESKJED_ONPREM = "privat-k9-dittnav-varsel-beskjed"
-    const val K9_DITTNAV_VARSEL_BESKJED_GCP = "dusseldorf.privat-k9-dittnav-varsel-beskjed"
+    const val K9_DITTNAV_VARSEL_BESKJED_AIVEN = "dusseldorf.privat-k9-dittnav-varsel-beskjed"
     const val DITT_NAV_BESKJED = "aapen-brukernotifikasjon-nyBeskjed-v1"
 }
