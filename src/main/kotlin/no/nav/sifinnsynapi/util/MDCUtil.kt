f15829eabecfe13907933e4cf6f0d10753b8ca55
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-10 13:47:11 +0200

    Fjerner ubrukt metoder

--- src/main/kotlin/no/nav/sifinnsynapi/util/MDCUtil.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/util/MDCUtil.kt
@@ -5,22 +5,6 @@ import org.slf4j.MDC
 import java.util.*
 
 object MDCUtil {
-    private val GEN = CallIdGenerator()
-    @JvmStatic
-    fun callId(): String {
-        return MDC.get(CORRELATION_ID)
-    }
-
-    fun callIdOrNew(): String {
-        return Optional.ofNullable(callId()).orElse(GEN.create())
-    }
-
-    fun toMDC(key: String?, value: Any?) {
-        if (value != null) {
-            toMDC(key, value.toString())
-        }
-    }
-
     @JvmOverloads
     fun toMDC(key: String?, value: String?, defaultValue: String? = null) {
         MDC.put(key, Optional.ofNullable(value)
