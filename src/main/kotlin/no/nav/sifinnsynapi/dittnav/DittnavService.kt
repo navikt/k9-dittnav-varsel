d933510de847b8b9877c2bf61d17b1e82fa25baf
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-10 11:03:08 +0200

    Logger når beskjed blir sendt videre til brukernotifikasjon

--- src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavService.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavService.kt
@@ -4,6 +4,7 @@ import no.nav.brukernotifikasjon.schemas.Beskjed
 import no.nav.brukernotifikasjon.schemas.Nokkel
 import no.nav.sifinnsynapi.config.Topics.DITT_NAV_BESKJED
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Service
 class DittnavService(
     private val onpremKafkaTemplate: KafkaTemplate<Nokkel, Beskjed>
 ) {
-
+    companion object{
+        val logger = LoggerFactory.getLogger(DittnavService::class.java)
+    }
     fun sendBeskjed(nøkkel: Nokkel, beskjed: Beskjed) {
+        logger.info("Sender beskjed videre til ${DITT_NAV_BESKJED} med eventId ${nøkkel.getEventId()}")
         onpremKafkaTemplate.send(
             ProducerRecord(
                 DITT_NAV_BESKJED,
