2d2f7136ddd6db459c573bcc1e718d098929a212
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-06-28 08:43:40 +0200

    Oppgraderer Spring og plugins. (#31)
    - Closes #28
    - Closes #29
    - Closes #30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.0"
+    id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.spring") version "1.5.10"
+    kotlin("jvm") version "1.5.20"
+    kotlin("plugin.spring") version "1.5.20"
 }
 
 group = "no.nav"
