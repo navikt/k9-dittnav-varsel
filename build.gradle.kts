a18484e35aeb5ee1124cf6aff7bbb647c74baf63
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-05-10 10:42:21 +0200

    Downgrader spring boot
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.spring") version "1.8.10"
-    id("org.springframework.boot") version "3.0.6"
+    id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.sonarqube") version "4.0.0.2929"
     jacoco
