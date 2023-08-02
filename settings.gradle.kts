07fa2b9845adf6f0c9b56b042b14e183a27c1303
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-08-02 08:23:48 +0200

    Reusable workflows, gradle bump (#167)
    * Reusable workflows, gradle bump
    
    * Reusable workflows, gradle bump
    
    * Dependabot groups, head branch er master

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "k9-dittnav-varsel"
+
+plugins {
+    id("com.gradle.enterprise") version("3.13.4")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
