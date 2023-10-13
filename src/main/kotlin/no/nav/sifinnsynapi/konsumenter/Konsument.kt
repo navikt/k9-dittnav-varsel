4864fb8cf2ab127ebc357d888def01180720a820
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-10-13 09:31:06 +0200

    Fjerner clientIdPrefix
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/konsumenter/Konsument.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/konsumenter/Konsument.kt
@@ -53,7 +53,6 @@ class Konsument(
         topics = [K9_DITTNAV_VARSEL_MICROFRONTEND],
         id = "k9-dittnav-varsel-microfrontend-aiven-listener",
         groupId = "#{'\${kafka.aiven.consumer.group-id}'}",
-        clientIdPrefix = "microfrontend-",
         containerFactory = "microfrontendKafkaJsonListenerContainerFactory",
     )
     fun konsumerMikrofrontendEvent(@Payload k9Microfrontend: K9Microfrontend) {
