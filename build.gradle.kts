65c346825f5e8c5de47c6e3552a9368af9aeac94
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-05-08 14:46:53 +0200

    Update Spring Boot plugin version to 4.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.3.20"
     kotlin("plugin.spring") version "2.3.20"
-    id("org.springframework.boot") version "4.0.5"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "7.2.3.7755"
     jacoco
